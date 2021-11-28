f861d0c023b1ec48627c5fa47f2084a652a626a8
Evgeniy <85104881+EVG777-prog@users.noreply.github.com>
2021-11-28 21:25:37 +0200

    fix - add semantic tags

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ var createNewTaskElement = function (taskString) {
   editButton.className = "button edit";
 
   deleteButton.className = "button delete";
+  deleteButtonImg.className = 'image';
   deleteButtonImg.src = './remove.svg';
   deleteButton.appendChild(deleteButtonImg);
 
